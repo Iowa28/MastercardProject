5a9de3275ad7cdd6b05424cbc91825851516f670
Iowa28 <yazzzik@list.ru>
2025-04-13 09:14:36 +0300

    init project

---
+++ src/main/java/ru/aminovniaz/mastercardproject/MastercardProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.aminovniaz.mastercardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastercardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MastercardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/aminovniaz/mastercardproject/MastercardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aminovniaz.mastercardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastercardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
